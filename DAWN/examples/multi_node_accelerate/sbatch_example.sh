ef0f37bdec8146b616e99ad4b8b172a66a966ec4
David Llewellyn-Jones <dllewellyn-jones@turing.ac.uk>
2024-02-16 22:23:44 +0000

    Provide script for building the conda environment
    Provides a script that can be used to create the conda environment using a
    compute node.

--- DAWN/examples/multi_node_accelerate/sbatch_example.sh
+++ DAWN/examples/multi_node_accelerate/sbatch_example.sh
@@ -10,10 +10,14 @@
 set -o errexit
 
 # Set up the environment.
+module load intelpython-conda/2024.0.1.3
 module load intel-oneapi-tbb/2021.11.0/oneapi/xtkj6nyp
 module load intel-oneapi-compilers/2024.0.0/gcc/znjudqsi
 module load intel-oneapi-mkl/2024.0.0/oneapi/4n7ruz44
 module load intel-oneapi-mpi/2021.11.0/oneapi/h7nq7sah
+
+# Activate conda environment
+conda activate torch-env
 
 # Avoid too many open file handles error.
 ulimit -n 1000000
@@ -31,9 +35,17 @@ export CCL_ZE_IPC_EXCHANGE=sockets
 # So that accelerate loads the ccl backend.
 export CCL_WORKER_COUNT=1
 
+# Capture usage
+xpu-smi discovery --dump 1,2,16,19
+stdbuf -o0 vmstat -t 1 -y > "cpu-${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}".txt &
+stdbuf -o0 xpu-smi dump -m 0,1,2 -i 1 > "dmon-${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}".txt &
+
 # Check that we can call mpirun with something simple.
 mpirun -n 16 -ppn 8 -prepend-rank hostname
 
 # Do training.
 /usr/bin/time mpirun -n 16 -ppn 8 -prepend-rank accelerate launch --config_file xpu-multinode.yaml example.py
+
+# Deactivate conda environment
+conda deactivate
 
